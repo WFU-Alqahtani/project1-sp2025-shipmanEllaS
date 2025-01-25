577ac968ee2c8aa61249003498637aca158af93d
shipmanEllaS <shipes24@wfu.edu>
2025-01-24 19:59:26 -0500

    setUpStore(), currentCart(), printReceiptOrder(), and emptyCartReverseOrder() are all created and function as intended. Presently, all exception identifications will cause the program to stop. To fix, remove the offending input from the items in the cart and process as normal.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,124 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("fix me!");
+    public void main(String[] args) {
+        Item[] store = setUpStore();
+        ArrayList<Item> currentCart = createCart(store, args);
+        printReceiptOrder(currentCart);
+        emptyCartReverseOrder(currentCart);
     }
+
+    /*  --------------------------------------------------------------------------------------
+     *   setUpSore - returns an array of Items that can be found in the store
+     */
+    public Item[] setUpStore() {
+        //Create array and add elements
+        Item[] itemsInStore = new Item[5];
+        itemsInStore[0] = new Item("Merino Yarn", 23.50);
+        itemsInStore[1] = new Item("Cotton Yarn", 7.50);
+        itemsInStore[2] = new Item("Silk Yarn  ", 20.75);
+        itemsInStore[3] = new Item("Bamboo Yarn", 13.50);
+        itemsInStore[4] = new Item("Linen Yarn ", 10.25);
+
+        //Return array
+        return itemsInStore;
+    }
+    //  --------------------------------------------------------------------------------------
+
+    /*  --------------------------------------------------------------------------------------
+     *   createCart - returns an arraylist of Items currently in the cart
+     *   itemsInStore : an array of the items available for purchase
+     */
+    public ArrayList<Item> createCart (Item[] itemsInStore, String[] args) {
+        //Create  array list of cart items
+        ArrayList<Item> cart = new ArrayList<Item>();
+
+        //Check to see how many items will be added to cart
+        int howManyTotalItems = -1;
+        try {
+            howManyTotalItems = Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Your cart is empty!");
+            System.exit(1);
+        } catch (NumberFormatException e2) {
+            System.out.println("You cannot have \"" + args[0] + "\" items.");
+            System.exit(1);
+        }
+        if (args.length != (howManyTotalItems + 1)) {
+            System.out.println("You are missing items from your cart!");
+        }
+
+        //Process and add items from args
+        for (int i = 1; i <= howManyTotalItems; i++) {
+            //Identify item
+            int currentItem = -1;
+            try {
+                currentItem = Integer.parseInt(args[i]);
+                cart.add(itemsInStore[currentItem]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("The store does not have an item at index " + args[i] + ".");
+                System.exit(1);
+            } catch (NumberFormatException e2) {
+                System.out.println("\"" + args[i] + "\" is not an accepted integer.");
+                System.exit(1);
+            }
+        }
+
+        //Return array list
+        return cart;
+    }
+    //  --------------------------------------------------------------------------------------
+
+    /*  --------------------------------------------------------------------------------------
+     *   printReceiptOrder - prints a receipt of all items, prices, subtotal, tax, and total
+     *   cart : list of items currently in the cart
+     */
+    public void printReceiptOrder(ArrayList<Item> cart) {
+        //Establishing values
+        double subtotal = 0.0;
+        double total = 0.0;
+
+        //Print header
+        System.out.println("Receipt");
+        System.out.println("=========================");
+        System.out.println("Item                Price");
+
+        //Print all items and add to subtotal
+        for (int i = 0; i < cart.size(); i++) {
+            System.out.print(cart.get(i).getItemName());
+            System.out.print("         ");
+            System.out.printf("%5.2f\n", cart.get(i).getItemPrice());
+
+            subtotal += cart.get(i).getItemPrice();
+        }
+        System.out.println("=========================");
+
+        //Calculate total
+        total = subtotal * 1.05;
+
+        //Printing subtotal, tax, and total
+        System.out.print("(a) Subtotal: ");
+        System.out.printf("%.2f\n", subtotal);
+        System.out.println("(b) Sales Tax: 5%");
+        System.out.print("(c) Total: ");
+        System.out.printf("%.2f\n", total);
+    }
+    //  --------------------------------------------------------------------------------------
+
+    /*  --------------------------------------------------------------------------------------
+     *   emptyCartReverseOrder - Displays and removes all items in the cart in reverse order
+     *   cart : list of items currently in the cart
+     */
+    public void emptyCartReverseOrder(ArrayList<Item> cart) {
+        System.out.println("Removing all items from the cart in reverse order...");
+
+        for (int i = (cart.size() -1); i >= 0; i--) {
+            System.out.println("Removing: " + cart.get(i).getItemName());
+            cart.remove(i);
+        }
+
+        System.out.println("Your cart has been emptied.");
+    }
+    //  --------------------------------------------------------------------------------------
 }
